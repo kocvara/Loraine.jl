85a6b543bdc674171ff98112cb44997bea0eaeb9
Michal Kocvara <kocvara@penopt.com>
2023-06-28 23:32:10 +0100

    docs

---
+++ docs/src/overview.md
@@ -0,0 +1,8 @@
+Linear SDP problem
+
+```math
+\begin{align}
+\min c^Tx 
+\end{align}
+```
+asa ``x``
--- examples/solve_sdpa.jl
+++ examples/solve_sdpa.jl
@@ -33,15 +33,15 @@ set_attribute(model, "verb", 1)
 set_attribute(model, "datarank", -1)
 set_attribute(model, "initpoint", 0)
 set_attribute(model, "maxit", 100)
-set_attribute(model, "datarank", -1)
+set_attribute(model, "datarank", 0)
 
 optimize!(model)
 
 # Mosek (CSDP, etc) for a comparison
 # Mosek must solve the dualized problem to be efficient
 
-# set_optimizer(model, Mosek.Optimizer)
-# dual_model = dualize(model, Mosek.Optimizer)
-# optimize!(dual_model)
+set_optimizer(model, CSDP.Optimizer)
+dual_model = dualize(model, CSDP.Optimizer)
+optimize!(dual_model)
 
 # termination_status(model)
