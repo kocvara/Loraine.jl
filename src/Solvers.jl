57cd0f0ddd19f4cbfe4f431d45d8d44ac24ba6da
Michal Kocvara <kocvara@penopt.com>
2024-08-17 14:13:12 +0100

    v025

--- src/Solvers.jl
+++ src/Solvers.jl
@@ -241,7 +241,7 @@ function load(model, options::Dict; T = Float64)
 
     if verb > 0
         t1 = time()
-        @printf("\n *** Loraine.jl v0.2.4 ***\n")
+        @printf("\n *** Loraine.jl v0.2.5 ***\n")
         @printf(" *** Initialisation STARTS\n")
     end
 
