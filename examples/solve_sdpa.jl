dabf8ce7cc03e19d46db8379ef4c6613d26769a6
Michal Kocvara <kocvara@penopt.com>
2026-06-07 00:05:26 +0100

    t

--- examples/solve_sdpa.jl
+++ examples/solve_sdpa.jl
@@ -26,21 +26,21 @@ end
 
 # Select your semidefinite optimization problem in SDPA input format
 # model=read_from_file("/Users/michal/Dropbox/michal/sdplib/Hans/trto2.dat-s")
-model=read_from_file("/Users/michal/Dropbox/michal/sdplib/thetaG11.dat-s")
+model=read_from_file("/Users/michal/Dropbox/michal/sdplib/maxG11.dat-s")
 # model=read_from_file("/Users/michal/Dropbox/michal/POEMA/IP/ip-for-low-rank-sdp/database/problems/SDPA/vib5.dat-s")
 # model=read_from_file("/Users/michal/Dropbox/michal/j/k.dat-s")
 
 # model=read_sdpa("examples/data/theta1.dat-s")
 # model=read_from_file(joinpath(dirname(@__DIR__), "examples/data/theta1.dat-s"))
 # model=read_from_file("examples/data/theta1.dat-s")
-# model=read_from_file("examples/data/maxG11.dat-s") #use with "datarank = -1"
+model=read_from_file("examples/data/vib9.dat-s") #use with "datarank = -1"
 
 set_optimizer(model, Loraine.Optimizer{Float64})
 # set_optimizer(model, Loraine.Optimizer{Float64x2})
 
 # Loraine options
 
-set_attribute(model, "kit", 0)
+set_attribute(model, "kit", 1)
 set_attribute(model, "tol_cg", 1.0e-2)
 set_attribute(model, "tol_cg_min", 1.0e-6)
 set_attribute(model, "eDIMACS", 1e-6)
