2f2bdf737ab4e43f97fb806a050e0a5de6d13fe6
Michal Kocvara <kocvara@penopt.com>
2026-08-03 18:02:43 +0100

    Bump version to 0.2.8

--- src/Solvers.jl
+++ src/Solvers.jl
@@ -246,7 +246,7 @@ function load(model, options::Dict; T = Float64)
 
     if verb > 0
         t1 = time()
-        @printf("\n *** Loraine.jl v0.2.7 ***\n")
+        @printf("\n *** Loraine.jl v0.2.8 ***\n")
         @printf(" *** Initialisation STARTS\n")
     end
 
